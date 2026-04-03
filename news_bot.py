3f651e8660909ad368b58ea1616e0ca45c6590d9
siphen00 <33291287+siphen00@users.noreply.github.com>
2026-04-03 16:11:43 +0300

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -5,28 +5,53 @@ import os
 TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-feeds = [
-    "https://cointelegraph.com/rss",
-    "https://www.coindesk.com/arc/outboundfeeds/rss/",
-    "https://www.reuters.com/markets/rss"
+feeds = {
+
+"🚨 MACRO":
+[
+"https://www.reuters.com/markets/rss",
+"https://www.ft.com/world?format=rss"
+],
+
+"📊 CRYPTO":
+[
+"https://cointelegraph.com/rss",
+"https://www.coindesk.com/arc/outboundfeeds/rss/"
+],
+
+"🏦 CENTRAL BANK / ECONOMY":
+[
+"https://www.investing.com/rss/news_25.rss"
 ]
+
+}
 
 sent_links = set()
 
-for url in feeds:
-    feed = feedparser.parse(url)
+for category in feeds:
 
-    for entry in feed.entries[:3]:
+    for url in feeds[category]:
 
-        message = f"""
-🚨 Trading News Alert
+        feed = feedparser.parse(url)
+
+        for entry in feed.entries[:2]:
+
+            if entry.link not in sent_links:
+
+                message = f"""
+{category}
 
 {entry.title}
 
 {entry.link}
 """
 
-        requests.post(
-            f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-            data={"chat_id": CHANNEL_ID, "text": message}
-        )
+                requests.post(
+                    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+                    data={
+                        "chat_id": CHANNEL_ID,
+                        "text": message
+                    }
+                )
+
+                sent_links.add(entry.link)
