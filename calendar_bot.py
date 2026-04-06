5f8899d6603c87bdc6cbe4e6c2dddbe004186477
siphen00 <33291287+siphen00@users.noreply.github.com>
2026-04-06 13:52:59 +0300

    Update calendar_bot.py

--- calendar_bot.py
+++ calendar_bot.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
@@ -25,7 +25,6 @@ RED_FOLDER_KEYWORDS = [
 "Jobless Claims",
 "Powell",
 "Federal Reserve"
-
 ]
 
 
@@ -53,26 +52,29 @@ def pin_message(message_id):
     )
 
 
-today = datetime.utcnow().strftime("%Y-%m-%d")
-
 events = requests.get(API_URL).json()
+
+today = datetime.utcnow().date()
+tomorrow = today + timedelta(days=1)
 
 today_events = []
 
 
 for event in events:
 
-    event_date = event["Date"][:10]
+    event_datetime = datetime.fromisoformat(event["Date"].replace("Z", ""))
+
+    event_date = event_datetime.date()
 
     title = event["Event"]
 
-    if event_date == today:
+    if today <= event_date <= tomorrow:
 
         if any(keyword.lower() in title.lower() for keyword in RED_FOLDER_KEYWORDS):
 
-            time = event["Date"][11:16]
+            event_time = event_datetime.strftime("%H:%M")
 
-            today_events.append(f"{time} — {title}")
+            today_events.append(f"{event_time} — {title}")
 
 
 if today_events:
